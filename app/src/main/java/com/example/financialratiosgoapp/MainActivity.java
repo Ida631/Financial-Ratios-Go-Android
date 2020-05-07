87829aab67528b96c4a66ccf7a347f1f2b378b76
Ida631 <52692119+Ida631@users.noreply.github.com>
2020-05-06 22:42:47 -0400

    initial commit

---
+++ app/src/main/java/com/example/financialratiosgoapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.financialratiosgoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
---
+++ app/src/main/java/com/example/financialratiosgoapp/MainAdapter.java
@@ -0,0 +1,16 @@
+package com.example.financialratiosgoapp;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainAdapter {
+}
